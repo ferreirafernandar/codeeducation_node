e599fd5d86d78db5a2e22fed45ff70c7dde5d407
Fernanda Ferreira <fernandaferreira@L3-000424.local>
2022-04-03 14:30:21 -0300

    corrige resposta

--- node/index.js
+++ node/index.js
@@ -15,7 +15,7 @@ connection.query(sql);
 connection.end();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Full Cycle</h1>");
+  res.send("<h1>Full Cycle Rocks!</h1>");
 });
 
 app.listen(port, () => {

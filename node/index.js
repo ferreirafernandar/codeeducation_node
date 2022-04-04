10abf61a132e0902bf87fa53ae1a9eb4a2a1355f
Fernanda Ferreira <fernandaferreira@L3-000424.local>
2022-04-04 11:15:54 -0300

    muda insert

--- node/index.js
+++ node/index.js
@@ -10,7 +10,7 @@ const config = {
 const mysql = require("mysql");
 const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Adora')`;
+const sql = `INSERT INTO people(name) values('Adora Alice')`;
 connection.query(sql);
 
 app.get("/", (req, res) => {
